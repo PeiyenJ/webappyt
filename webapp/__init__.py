ae29efe647a246ad481696dd6b205c33c872c441
PeiyenJ <peiyenjen@outlook.com>
2023-12-17 20:13:17 +0800

    annotation update

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# 創一個新的SQLAlchemy物件
+# 創造SQLAlchemy (object)
 db = SQLAlchemy()
-# 一個variable去存放資料庫的名字
+
+# 一個variable去存放資料庫的名稱
 YOUTUBE_DATABASE = "youtube_database.db"
 
 def create_web_app():
@@ -13,13 +14,14 @@ def create_web_app():
 
         # 設置資料庫連結
         app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{YOUTUBE_DATABASE}'
+        # 初始化資料庫
         db.init_app(app)
 
         # 註冊藍圖.引入home模組
         from .home import home
         app.register_blueprint(home, url_prefix='/')
 
-        # 創建資料庫
+        # 創建資料庫,建立model
         with app.app_context():
                 db.create_all()
 
--- webapp/home.py
+++ webapp/home.py
@@ -5,12 +5,13 @@ from . import db
 
 home = Blueprint('home',__name__)
 
-# API，預設GET，因此需要增加list方法
+# API，預設GET，因此需要增加list存放兩種method->methods
 @home.route('/',methods=["GET","POST"])
 
 def download_youtube_video():
+        
         if request.method =="POST":
-                # 把用戶在前端的網址取出
+                # 把用戶在前端的存在form的網址取出
                 video_url = request.form.get("downloadUrl")
 
                 # 用取出來的網址 創造物件
@@ -29,12 +30,12 @@ def download_youtube_video():
                 # 下載影片到伺服器
                 get_video = youtube_video_object.streams.get_highest_resolution()
                 
-                # 用戶端下載存放
+                # 用戶端下載存放， as_attachment是個附件
                 return send_file(get_video.download(), as_attachment=True)
 
         # 如果不是POST 就是GET
         else:
-                   # 讀取資料庫
+                # 讀取資料庫
                 videos = Video.query.all()
                 # 把從資料庫讀取出來的資料送到前端
                 return render_template("home.html", videos=videos)
